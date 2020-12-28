af982381cdbbb67b15f7d51d8b2fb635e83b1d85
thtiiz <thitiwat.tha@ku.th>
2020-12-28 15:10:36 +0700

    fix navigator on appscreen

--- lib/presentation/pages/app_screen.dart
+++ lib/presentation/pages/app_screen.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:spent/di/di.dart';
@@ -10,12 +13,13 @@ import 'package:spent/presentation/pages/bookmark_page.dart';
 import 'package:spent/presentation/pages/following_page.dart';
 import 'package:spent/presentation/pages/home_page.dart';
 import 'package:spent/presentation/pages/search_page.dart';
-import 'package:spent/presentation/widgets/app_retain_widget.dart';
 
 import 'package:spent/presentation/widgets/bottom_navbar.dart';
 import 'package:spent/presentation/widgets/nav_drawer.dart';
 
 class AppScreen extends StatelessWidget {
+  final _channel = const MethodChannel('com.example.spent/app_retain');
+
   final ScrollController _scrollController = ScrollController();
 
   final PageController _pageController = PageController(
@@ -25,7 +29,6 @@ class AppScreen extends StatelessWidget {
   AppScreen({Key key}) : super(key: key);
 
   void _onPageChanged(BuildContext context, int index) {
-    print(index);
     NavItem item = NavItem.values[index];
     BlocProvider.of<NavigationBloc>(context).add(NavigateTo(item));
   }
@@ -34,13 +37,38 @@ class AppScreen extends StatelessWidget {
     Navigator.push(context, CupertinoPageRoute(builder: (context) => SearchPage()));
   }
 
+  Future<bool> onWillPop(BuildContext context) async {
+    try {
+      final _pageController = BlocProvider.of<NavigationBloc>(context).pageController;
+      if (_pageController.page.round() == _pageController.initialPage) {
+        if (Platform.isAndroid) {
+          print('pop');
+          if (Navigator.of(context).canPop()) {
+            return true;
+          } else {
+            _channel.invokeMethod('sendToBackground');
+            return false;
+          }
+        } else {
+          return true;
+        }
+      } else {
+        _onPageChanged(context, 0);
+        return false;
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return AppRetainWidget(
-      child: BlocProvider<NavigationBloc>(
-        create: (BuildContext context) => getIt<NavigationBloc>(param1: _pageController),
-        child: BlocBuilder<NavigationBloc, NavigationState>(
-          builder: (BuildContext context, NavigationState state) => Scaffold(
+    return BlocProvider<NavigationBloc>(
+      create: (BuildContext context) => getIt<NavigationBloc>(param1: _pageController),
+      child: BlocBuilder<NavigationBloc, NavigationState>(
+        builder: (BuildContext context, NavigationState state) => WillPopScope(
+          onWillPop: () => onWillPop(context),
+          child: Scaffold(
             drawer: NavDrawer(),
             appBar: AppBar(
               title: Text(
